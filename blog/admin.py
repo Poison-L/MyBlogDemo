1b365c29a92cca87417dbac6656bce07a25c6fd5
Robert <lichunjin521@gmail.com>
2018-01-23 12:53:05 +0800

    更改admin登录几面header 和 title

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
 
+admin.site.site_title = u'博客管理'
+admin.site.site_header = u'博客管理系统'
 
+
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
 
 
-admin.site.register(Post, PostAdmin)
-admin.site.register(Category)
-admin.site.register(Tag)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
